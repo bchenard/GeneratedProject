1454684bb8bada92dcea05fe4fa51970f01614f4
Benoît Chénard <117839483+bchenard@users.noreply.github.com>
2025-01-10 15:13:31 +0100

    Add source code

---
+++ folder/subfolder/frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
