15b0225726641326731188fcff9decb2382f8b0a
Benoît Chénard <117839483+bchenard@users.noreply.github.com>
2025-01-10 15:13:07 +0100

    Add source code

---
+++ folder/subfolder/backend/src/Nebulift.Api/Program.cs
@@ -0,0 +1,36 @@
+namespace Nebulift.Api;
+
+/// <summary>
+/// The main program class.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    /// <param name="args">The command-line arguments.</param>
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
