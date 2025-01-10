10f2bb94527b09e7acd45404a2fe51fc474a8b1a
Benoît Chénard <117839483+bchenard@users.noreply.github.com>
2025-01-10 15:13:19 +0100

    Add source code

---
+++ folder/subfolder/frontend/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+console.log('Hello from the frontend!');
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </ClientOnly>
+  </div>
+</template>
